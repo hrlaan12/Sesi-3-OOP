a8a11c9e5321695b45bea4c9ea676bb162ee0468
hrlaan12 <ujang.herlan_ti22@nusaputra.ac.id>
2024-03-01 16:42:04 +0700

    git commit -m "tambahan"

--- Inheritance/src/App.java
+++ Inheritance/src/App.java
@@ -1,11 +1,24 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        Lingkaran lingkaran  = new Lingkaran(7, "Lingkaran 123");
+    public static void main(String[] args) {
+        Lingkaran lingkaran = new Lingkaran(7, "Lingkaran 123");
         lingkaran.getInfo();
 
-        //Segitiga
-        Segitiga segitiga = new Segitiga(1,3, "Segitiga 123");
+        // Segitiga
+        Segitiga segitiga = new Segitiga(10, 8, 5, 3, 4, "Segitiga 123");
         segitiga.getInfo();
+
+        //sikusiku
+        SegitigaSikuSiku segitigaSikuSiku = new SegitigaSikuSiku(3, 4, 5,  "Segitiga Siku-Siku 123");
+        segitigaSikuSiku.getInfo();
+
+        //segitigasembarang
+        SegitigaSembarang segitigaSembarang = new SegitigaSembarang(3, 4, 5, 6, 7, "Segitiga Sembarang");
+        segitigaSembarang.getInfo();
+        
+        //segitigasembarang
+        SegitigaSamaSisi segitigaSamaSisi = new SegitigaSamaSisi(3, "Sama Sisi");
+        segitigaSamaSisi.getInfo();
+
+
     }
 }
-//PPPPRRRRRR
